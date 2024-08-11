eb225766fcafe6350491ddfdadd4bb82af3f6b70
hnthap <94937751+hnthap@users.noreply.github.com>
2024-08-11 20:52:17 +0700

    make code more readable + clean up .gitignore

--- backup_helper.py
+++ backup_helper.py
@@ -10,26 +10,15 @@ def extract_arguments():
         description='Help you quickly create backup as compressed directory',
         epilog='Have fun backing up'
     )
-    parser.add_argument('outdir', action='store_const', const='backup')
     parser.add_argument('directories', nargs='+')
+    parser.add_argument('-o', '--outdir')
     parser.add_argument('-p', '--password')
     args = parser.parse_args()
     return args
 
 
-def parse_arguments():
-    args = extract_arguments()
-    outdir = Path(args.outdir)
-    if not outdir.exists():
-        outdir.mkdir()
-    directories = [Path(d) for d in args.directories]
-    assert all(d.exists() for d in directories)
-    password = args.password
-    return outdir, directories, password
-
-
 def new_backup(outdir: Path, directory: Path, password: Union[str, None]):
-    print('Compressing {}'.format(directory))
+    print('\033[92m[INFO] Compressing {}\033[0m'.format(directory))
     out_name = directory.name
     out_path = outdir / '{}.7z'.format(out_name)
     subprocess.run([
@@ -40,3 +29,25 @@ def new_backup(outdir: Path, directory: Path, password: Union[str, None]):
         '-p' if password is None else '-p{}'.format(password), 
         '-mhe=on'
     ])
+
+
+def parse_arguments():
+    args = extract_arguments()
+    outdir = parse_arg_outdir(args)
+    directories = parse_arg_directories(args)
+    password = args.password
+    return outdir, directories, password
+
+
+def parse_arg_directories(args: argparse.Namespace):
+    directories = [Path(d) for d in args.directories]
+    assert all(d.exists() for d in directories)
+    return directories
+
+
+def parse_arg_outdir(args: argparse.Namespace):
+    outdir = Path('backup' if args.outdir is None else args.outdir)
+    if not outdir.exists():
+        outdir.mkdir()
+    return outdir
+
